bb08ba0371d7aacc1792b01e45fb6b79abd6bfd9
Kevin Koech <kevinkoech@proton.me>
2023-04-15 13:42:22 +0300

    created a program that prints names of the authors in this repo

---
+++ authors.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+int main()
+{
+	char author1[] = "Kevin Koech";
+	char author2[] = "Daniel Korir";
+
+	printf("Authors: \n");
+	printf("\n1. %s\n", author1);
+	printf("\n2. %s\n", author2);
+	printf("\n");
+
+	return (0);
+}
+
