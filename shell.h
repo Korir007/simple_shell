bde26d8013fd8742957a305cd361628b5f61a1d4
Kevin Koech <kevinkoech@proton.me>
2023-04-20 15:29:14 +0300

    header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* C libraries required */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+/* Global variables */
+#define COMMAND_LENGTH 75
+
+#endif /* SHELL_H */
+
