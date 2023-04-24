0a66d1b6bcebc9e6740fc30f3d918ce3d942d5f5
Kevin Koech <kevinkoech@proton.me>
2023-04-24 10:49:22 +0300

    added string lib for strcmp function

--- shell.h
+++ shell.h
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <string.h>
 
-/* Global variables */
+/* Global variables */i
+
 #define COMMAND_LENGTH 75
 
 #endif /* SHELL_H */
