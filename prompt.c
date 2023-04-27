4e64619b201518ba9fc3698e551c2a19024b60eb
Korir007 <danielkorir303@gmail.com>
2023-04-26 17:49:44 -0700

    added a function

--- prompt.c
+++ prompt.c
@@ -18,3 +18,58 @@ void print_prompt(void)
 	}
 }
 
+/**
+ * _iswhich - locates a command
+ *
+ * @cmd: command name
+ *
+ * @_environ: environment variable
+ *
+ * Return: Exec of command
+ */
+
+char *_iswhich(char *command, char **_environ)
+{
+	char *path, *ptr_path, *token_path, *dir;
+	int len_dir, len_cmd, i;
+	struct stat st;
+
+	path = _getenv("PATH", _environ);
+	if (path)
+	{
+		ptr_path = _strdup(path);
+		len_cmd = _strlen(cmd);
+		token_path = _strtok(ptr_path, ":");
+		i = 0;
+		while (token_path != NULL)
+		{
+			if (is_cdir(path, &i))
+				if (stat(cmd, &st) == 0)
+					return (cmd);
+			len_dir = _strlen(token_path);
+			dir = malloc(len_dir + len_cmd + 2);
+			_strcpy(dir, token_path);
+			_strcat(dir, "/");
+			_strcat(dir, cmd);
+			_strcat(dir, "\0");
+			if (stat(dir, &st) == 0)
+			{
+				free(ptr_path);
+				return (dir);
+			}
+			free(dir);
+			token_path = _strtok(NULL, ":");
+		}
+		free(ptr_path);
+		if (stat(cmd, &st) == 0)
+			return (cmd);
+		return (NULL);
+	}
+	if (cmd[0] == '/')
+		if (stat(cmd, &st) == 0)
+			return (cmd);
+	return (NULL);
+}
+
+
+
