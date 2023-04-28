8eee9d0eb0b070d490487f807f5692a09ae54c76
Korir007 <danielkorir303@gmail.com>
2023-04-28 13:13:50 -0700

    minor fix

--- exit_shell.c
+++ exit_shell.c
@@ -13,16 +13,16 @@
  */
 void exit_shell(char *args[])
 {
-    int exit_status = 0; // Initialize the exit status to 0
+    int exit_msg = 0; // Initialize the exit status to 0
     
     // If an exit status is provided as an argument, convert it to an integer using atoi
     if (args[1] != NULL)
     {
-        exit_status = atoi(args[1]);
+        exit_msg = atoi(args[1]);
     }
     
     // Exit the shell with the specified exit status
-    exit(exit_status);
+    exit(exit_msg);
 }
 
 
