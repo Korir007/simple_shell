fee129c8d934ec76507e7c50807f7ad5dc90f385
Korir007 <danielkorir303@gmail.com>
2023-04-29 07:30:22 -0700

    more change

--- shell.h
+++ shell.h
@@ -14,19 +14,19 @@
 
 /* Global variables */
 
-#define COMMAND_LENGTH 75
-#define BUFFER 1024
-#define NAME "hsh"
-#define PATH 20
-#define ARGUMENTS 30
-#define MINI_BUFFER 256
+#define MAX_CMD_LENGTH 1024
+#define MAX_PATHS 64
+#define PATH_DELIM ":"
+#define MAX_ARGS 64
+#define MAX_LINE_LENGTH 1024
+#define MAX_ARGS 64
 
 /* Function prototypes */
 
 char *search_path(char *cmd, char *paths[], int npaths);
-int _strcmp (char *s1, const char *s2);
-char *_getline(char **line, size_t *n, FILE *ptr);
-char *_strtok(char *str, const char *delim);
+int my_strcmp(char *s1, const char *s2);
+char get_input(char **cmd, size_t *n, FILE *ptr);
+char *my_strtok(char *str, const char *delim);
 char *_strchr(const char *s, int c);
 void exit_shell(char *args[]);
 
